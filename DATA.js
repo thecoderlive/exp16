665cb856c345b2d433e4edff8acc2f59b4c15c96
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-05 02:39:28 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "48042a75-73ad-48d9-9048-951c128318cc",
+    "home_feed": [
+        {
+            "id": "683d4cde-3856-41bc-8995-3fdc0b9bb55e",
+            "artist_name": "personal",
+            "home_carousel": [
+                {
+                    "id": "ff4d0244-78e5-45ec-bc85-61eeb125db33",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/180.png"
+                },
+                {
+                    "id": "ae1f63fc-c779-4589-bb81-02c0dcb284b2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/236.png"
+                },
+                {
+                    "id": "1c94ab9d-b2be-4bd0-8005-f49de92c9f79",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/355.png"
+                },
+                {
+                    "id": "5cf45196-ad78-4eca-a262-47e67bff30d6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/243.png"
+                },
+                {
+                    "id": "175f4cd2-852b-4b1f-8b71-cfc46e237159",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/321.png"
+                }
+            ],
+            "art_caption": "than",
+            "art_description": "Certain by party media how benefit. Physical your source several. Different yourself every whole four fund recognize."
+        },
+        {
+            "id": "5d7a98d4-d5bd-4889-b9c9-35e3b264acc8",
+            "artist_name": "cold",
+            "home_carousel": [
+                {
+                    "id": "ebdb16b5-b3c3-460e-a83b-1b8f30b06565",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/108.png"
+                },
+                {
+                    "id": "a3c1fee8-6ae6-41dd-b197-7b7c5886917f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/27.png"
+                },
+                {
+                    "id": "24237856-0661-460b-bed1-4665f601bbf7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/326.png"
+                },
+                {
+                    "id": "d38356a5-5508-456d-9bad-5c4ad945f82d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/282.png"
+                },
+                {
+                    "id": "e301ad64-7dbd-470e-9c16-d7109f9bd3af",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/333.png"
+                }
+            ],
+            "art_caption": "nation",
+            "art_description": "Also song letter once pull campaign. Onto military start during message local teach third. Type mention method cover ask. Officer dog life hotel."
+        },
+        {
+            "id": "00a8eb01-4318-4df6-a663-03dc9d58aef1",
+            "artist_name": "tell",
+            "home_carousel": [
+                {
+                    "id": "3fe19b80-ee9c-4e6c-9301-1fc82a58fb32",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/106.png"
+                },
+                {
+                    "id": "fcfcaa0a-44ad-4e0b-b57b-6e52df59c362",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/247.png"
+                },
+                {
+                    "id": "056827ae-35e1-4e4b-8751-328140edc8b6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/35.png"
+                },
+                {
+                    "id": "ba002869-fccf-4755-9052-376330dddb6d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/56.png"
+                },
+                {
+                    "id": "efc7cae0-f9aa-4bc3-9a7f-0d17b65ba6e4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/43.png"
+                }
+            ],
+            "art_caption": "someone",
+            "art_description": "Until authority explain use. East continue quickly near material generation. Issue event ability truth space approach."
+        },
+        {
+            "id": "20b87433-1aa4-4c73-bcfb-cbb5b91b3912",
+            "artist_name": "yes",
+            "home_carousel": [
+                {
+                    "id": "1be35abd-3a6c-499d-804d-0592fe46182b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/181.png"
+                },
+                {
+                    "id": "2699ef6f-00d5-49b6-960c-3fcedd9a6feb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/46.png"
+                },
+                {
+                    "id": "a5eb4a5b-7dfa-42f8-a8fa-fbdce9275ecd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/211.png"
+                },
+                {
+                    "id": "26732464-b418-41fd-acfb-7dce3285579a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/29.png"
+                },
+                {
+                    "id": "e1ddfd95-b30d-4b35-a113-eb11ad3d7fca",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/215.png"
+                }
+            ],
+            "art_caption": "trade",
+            "art_description": "Short consumer tonight majority up represent summer. Born wind stuff follow moment investment bag."
+        },
+        {
+            "id": "115461ac-1e26-4d19-9665-054bb13d4d91",
+            "artist_name": "can",
+            "home_carousel": [
+                {
+                    "id": "0185ec36-8af4-43db-89e9-a40a383e8a6e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/19.png"
+                },
+                {
+                    "id": "d424c0e7-9963-486e-818d-cb33ee4d62fa",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/194.png"
+                },
+                {
+                    "id": "01da83c0-8bf8-4b32-b51e-85c1b16e5ac7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/138.png"
+                },
+                {
+                    "id": "925547a7-edfb-466c-8d64-70a63a396ad4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/111.png"
+                },
+                {
+                    "id": "fecf535f-d73f-4c76-bfe9-8b5786d16b18",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/355.png"
+                }
+            ],
+            "art_caption": "citizen",
+            "art_description": "Paper hit spend raise certainly interview. Range modern nor cover environmental success but."
+        }
+    ]
+}
